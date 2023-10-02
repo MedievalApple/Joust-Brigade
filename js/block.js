21127f97e8fd24494cded8da0ec297973ee09a06
Medieval Apple <evergaming2500@gmail.com>
2023-10-02 18:09:17 -0400

    Made It Look Like Joust :D

--- js/block.js
+++ js/block.js
@@ -1,15 +1,16 @@
 class Block {
-    constructor(x, y, w, h) {
+    constructor(x, y, w, h, c) {
         this.position = new Vector(x, y);
         this.velocity = new Vector(0, 0);
         this.width = w;
         this.height = h;
+        this.color = c;
 
         mapObjects.push(this);
     }
 
     show () {
-        ctx.fillStyle = "blue";
+        ctx.fillStyle = this.color;
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
--- js/map.js
+++ js/map.js
@@ -1,3 +1,9 @@
-new Block(0, 200, 140, 30);
-new Block(350, 250, 200, 40);
-new Block(850, 180, 140, 30);
+//PlatForms
+new Block(0, 72, 49, 15, "red"); //1
+new Block(132, 99, 143, 22, "red"); //2
+new Block(403, 73, 77, 22, "red"); //3
+new Block(0, 225, 100, 17, "red"); //4
+new Block(165, 282, 103, 17, "red"); //5
+new Block(321, 205, 94, 25, "red"); //6
+new Block(409, 225, 71, 15, "red"); //7
+new Block(79, 388, 303, 100, "red"); //8
