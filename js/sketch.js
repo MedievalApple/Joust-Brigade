c8825178f24c3e1ad573ea398edbd0178c1e7691
NateDCoder <rubyandtwixie@gmail.com>
2023-10-04 20:29:02 -0400

    Added the fire and hand

--- js/sketch.js
+++ js/sketch.js
@@ -3,16 +3,15 @@ var ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
 var player = new Image();
 player.src = "/assets/sprite_sheet.png";
-var sprite1;
-var p
 var x = 0;
 var p = new Player(50, 310, 13*2, 18*2, "red");
 var AIs = [];
 var mapBlockCollision = [];
 var backgroundColor = "black"
 var mapRef = new Image()
+var fire;
+var hand;
 mapRef.src = "/assets/sprite_sheet/map/map.png"
-
 // On focus on canvas, hide cursor
 canvas.addEventListener("click", function () {
     //canvas.requestPointerLock();
@@ -48,7 +47,11 @@ function draw() {
 
     //Joust Map Example
     ctx.drawImage(mapRef, 0, 0, canvas.width,canvas.height)
-
+    fire.show(2, 28, 388-fire.images[0].height*fire.scalar)
+    fire.show(2, canvas.width-28, 388-fire.images[0].height*fire.scalar)
+    handPos = Vector.lerp(new Vector(28, 388), p.position.clone().add(p.height), hand.currentImage/hand.images.length);
+    hand.show(3, handPos.x, handPos.y-hand.images[Math.floor(hand.currentImage)].height*hand.scalar + p.height/2)
+    //hand.show(0.5, canvas.width-28,interpolate(Math.floor(hand.currentImage) / hand.images.length, 388, p.position.y+p.height)-hand.images[Math.floor(hand.currentImage)].height*hand.scalar)
     p.update()
     p.show()
 
@@ -117,6 +120,8 @@ function draw() {
 }
 
 player.onload = function () {
+    hand = new Sprite("/assets/sprite_sheet/lava_troll/troll", 5, null, 2);
+    fire = new Sprite("/assets/sprite_sheet/lava_troll/fire", 4, null, 2);
     for (let i = 0; i < 5; i++) {
         AIs[i] = new Enemy(Math.random() * canvas.width, 20, 13*2, 19*2, "green");
         switch (Math.floor(Math.random() * 2)) {
---
+++ js/utils.js
@@ -0,0 +1,7 @@
+function interpolate(t, start, end) {
+  // Make sure t is within the range [0, 1]
+  t = Math.max(0, Math.min(1, t));
+  
+  // Calculate the interpolated value
+  return start + (end - start) * t;
+}
