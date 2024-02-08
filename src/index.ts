83efb17573f36816a3013ae15d958135bb21c809
3xjn <mcphersonasher@gmail.com>
2024-02-07 19:29:45 -0500

    Revert "Revert "Added spectator flag and changed name of menu.css""
    This reverts commit 040099b312c5ff68e369f876347b4c447768eae9.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 function startGame() {
     const nameInput = <HTMLInputElement>document.getElementById("nameInput");
     const serverInput = <HTMLInputElement>document.getElementById("serverInput");
+    const checkbox = <HTMLInputElement>document.getElementById("spectatorCheckbox");
 
     const username = nameInput.value;
     const server = serverInput.value;
+    const spectator = checkbox.checked;
 
     if (server !== " ") {
         sessionStorage.setItem("server", server);
     }
 
+    sessionStorage.setItem("spectator", spectator.toString());
+
     if (username !== "" && username !== " ") {
         sessionStorage.setItem("username", username);
         window.location.replace("joust.html");
     }
-
 }
 
 document.addEventListener("DOMContentLoaded", () => {
